75f47331db59ef0e5a53e5ecaf2efd76ed23d1d3
Fábio <mckfabio@gmail.com>
2023-10-09 23:28:14 -0300

    Update to 3.0.7256
    Will eventually need auto-update.

---
+++ systeminformer-nightlybuilds3.0.7256/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageArgs = @{
+  packageName    = 'systeminformer-nightlybuilds'
+  fileType       = 'exe'
+  url            = "https://github.com/winsiderss/si-builds/releases/download/3.0.7256/systeminformer-3.0.7256-setup.exe"
+  softwareName   = 'System Informer'
+  checksum       = '2243762B0127BEA893983692AC7F20285ED9A9F5'
+  checksumType   = 'sha1'
+  silentArgs     = '-silent'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
