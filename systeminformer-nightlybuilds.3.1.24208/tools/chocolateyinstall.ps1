b8f935a23d042074e26674c76ae586f1a5f54b6f
Lemon Lam <alemonmk@users.noreply.github.com>
2024-08-16 01:59:15 +0800

    Update to 3.1.24208
    `-silent` switch is gone for so long: https://github.com/winsiderss/systeminformer/blame/eac06f6ea64788bafc2c4fb67e2e55ecb966a9ed/tools/CustomSetupTool/main.c#L395

---
+++ systeminformer-nightlybuilds.3.1.24208/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageArgs = @{
+  packageName    = 'systeminformer-nightlybuilds'
+  fileType       = 'exe'
+  url            = "https://github.com/winsiderss/si-builds/releases/download/3.1.24208/systeminformer-3.1.24208-release-setup.exe"
+  softwareName   = 'System Informer'
+  checksum       = '9183ED432BF82809B1F13D73EB5F0E44D91C3881887C47A28BDDB04356E8EC70'
+  checksumType   = 'sha256'
+#  silentArgs     = '-silent'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
