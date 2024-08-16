cc47507a0b61d5b99f1830ed98a7fb214b3e2d82
Lemon Lam <alemonmk@users.noreply.github.com>
2024-08-17 00:08:41 +0800

    Use portable package to eliminate user interaction

--- systeminformer-nightlybuilds.3.1.24208/tools/chocolateyinstall.ps1
+++ systeminformer-nightlybuilds.3.1.24208/tools/chocolateyinstall.ps1
@@ -1,12 +1,25 @@
-﻿$packageArgs = @{
-  packageName    = 'systeminformer-nightlybuilds'
-  fileType       = 'exe'
-  url            = "https://github.com/winsiderss/si-builds/releases/download/3.1.24208/systeminformer-3.1.24208-release-setup.exe"
-  softwareName   = 'System Informer'
-  checksum       = '9183ED432BF82809B1F13D73EB5F0E44D91C3881887C47A28BDDB04356E8EC70'
-  checksumType   = 'sha256'
-#  silentArgs     = '-silent'
-  validExitCodes = @(0)
+﻿$PackageSourceUrl = "https://github.com/winsiderss/si-builds/releases/download/3.1.24208/systeminformer-3.1.24208-release-bin.zip"
+$Architecture = if (Get-OSArchitectureWidth 32) { "i386" } else { "amd64" }
+$InstallDirectory =
+Join-Path `
+  -Path $(if (Get-OSArchitectureWidth 64) { $Env:ProgramW6432 } else { $Env:ProgramFiles }) `
+  -ChildPath "SystemInformer"
+
+$PackageParams = @{
+  PackageName    = "systeminformer-nightlybuilds"
+  UnzipLocation  = $InstallDirectory
+  Url            = $PackageSourceUrl
+  Checksum       = '86AA4237565375FF214368FA855B8304BC4675F4BD547F7C7EF654C3A9BE5FD1'
+  ChecksumType   = 'sha256'
+  SpecificFolder = $Architecture
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @PackageParams
+
+$ShortcutParams = @{
+  ShortcutFilePath = "$Env:PUBLIC\Desktop\System Informer.lnk"
+  TargetPath       = "$InstallDirectory\$Architecture\SystemInformer.exe"
+  IconLocation     = "$InstallDirectory\$Architecture\SystemInformer.exe"
+}
+
+Install-ChocolateyShortcut @ShortcutParams
---
+++ systeminformer-nightlybuilds.3.1.24208/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿Uninstall-ChocolateyZipPackage -Packagename $env:ChocolateyPackageName -ZipFileName "systeminformer-3.1.24208-release-bin.zip"
+
+$InstallDirectory =
+Join-Path `
+  -Path $(if (Get-OSArchitectureWidth 64) { $Env:ProgramW6432 } else { $Env:ProgramFiles }) `
+  -ChildPath "SystemInformer"
+if (Test-Path $InstallDirectory) { Remove-Item $InstallDirectory -Recurse }
+
+$ShortcutPath = "$Env:PUBLIC\Desktop\System Informer.lnk"
+if (Test-Path $ShortcutPath) { Remove-Item $ShortcutPath }
